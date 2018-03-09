e62edd8eebcf793312fb966937e24a273fad3cfc
sacitgonen <sacit.gonen41@myhunter.cuny.edu>
2018-03-09 17:42:47 -0500

    added sign and registration scenes

---
+++ MusicApp/Assets/scripts/SignIn/RegistrationPage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine;
+
+
+namespace SignIn{
+	public class RegistrationPage : MonoBehaviour {
+
+		public InputField email;
+		public InputField password;
+		public InputField fname;
+		public InputField lname;
+			
+		public void register_submit (string sceneName) {
+			FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(email.text, password.text).ContinueWith((obj) => {
+				SceneManager.LoadSceneAsync (sceneName);
+				});
+		}
+	}
+}
