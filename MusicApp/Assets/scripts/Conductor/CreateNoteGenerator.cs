d6aaf1e45936542487a8d227475c733a794e8414
shellyhuang18 <shellyhuang81@gmail.com>
2018-04-02 15:38:08 -0400

    dont ask

--- MusicApp/Assets/scripts/Conductor/CreateNoteGenerator.cs
+++ MusicApp/Assets/scripts/Conductor/CreateNoteGenerator.cs
@@ -52,7 +52,21 @@ namespace Conductor{
 			}
 
 			if (Input.GetKeyDown ("a")) {
+<<<<<<< HEAD
 				NoteLogic.NoteLogic.Song new_song = new NoteLogic.NoteLogic.Song ("8d4 8d#4");
+=======
+				NoteLogic.NoteLogic.Song new_song = new NoteLogic.NoteLogic.Song ("2d4 16e4");
+				StartCoroutine (startSong (new_song));
+
+			}
+			if (Input.GetKeyDown ("s")) {
+				NoteLogic.NoteLogic.Song new_song = new NoteLogic.NoteLogic.Song ("3d4 8e4");
+				StartCoroutine (startSong (new_song));
+
+			}
+			if (Input.GetKeyDown ("d")) {
+				NoteLogic.NoteLogic.Song new_song = new NoteLogic.NoteLogic.Song ("5d4 6r 4e4");
+>>>>>>> 8bfc232... notes are separated correctly by duration
 				StartCoroutine (startSong (new_song));
 
 			}
@@ -65,23 +79,43 @@ namespace Conductor{
 		 
 
 		IEnumerator startSong(NoteLogic.NoteLogic.Song new_song){
+<<<<<<< HEAD
 			//new_song.PrintScore ();
 			float timing = .51f;
 
 			foreach (NoteLogic.NoteLogic.Sound item in new_song.score) {
 				
+=======
+			float timing = .1f;
+			float pix_per_frame = tempo / 50;
+			float obj_width = 10;
+			float pix_req;
+
+			foreach (NoteLogic.NoteLogic.Sound item in new_song.score) {
+				obj_width = 10;
+>>>>>>> 8bfc232... notes are separated correctly by duration
 				//output chords
 				if (item.is_chord) {
+					
 					NoteLogic.NoteLogic.Chord d = item as NoteLogic.NoteLogic.Chord;
 					//timing = (float)(d.duration * 4) / 100;
 					Debug.Log (Time.deltaTime);
 					//output notes in chord
 					foreach (NoteLogic.NoteLogic.Note i in d.notes) {
-						triggerPitch (i.pitch, i.duration);
+						if (i.pitch != "r") {
+							triggerPitch (i.pitch, i.duration);
+						}
+
+						obj_width *= (float)(i.duration/16);
 					}
+
+				
+					pix_req = obj_width / pix_per_frame;
+					timing = pix_req * Time.deltaTime;
 				//output single notes
 				} else {
 					NoteLogic.NoteLogic.Note n = item as NoteLogic.NoteLogic.Note;
+<<<<<<< HEAD
 					//timing = (float)(n.duration * 4) / 100;
 					Debug.Log (Time.deltaTime);
 
@@ -89,10 +123,25 @@ namespace Conductor{
 				}
 				yield return new WaitForSeconds (timing);
 
+=======
+					if (n.pitch != "r") {
+						triggerPitch (n.pitch, n.duration);
+					}
+
+					obj_width *= (float)(n.duration / 16);
+
+					//Debug.Log (obj_width);
+					pix_req = obj_width / pix_per_frame;
+					timing = pix_req * Time.deltaTime;
+
+				}
+
+				yield return new WaitForSeconds (timing);
+>>>>>>> 8bfc232... notes are separated correctly by duration
 			}
 		}
 
-		void triggerPitch(string pitch, int duration){
+		void triggerPitch(string pitch, float duration){
 			GameObject note_spawner = GameObject.Find (pitch);
 			note_spawner.GetComponent<GenerateNotes>().generateNote(duration);
 		}
