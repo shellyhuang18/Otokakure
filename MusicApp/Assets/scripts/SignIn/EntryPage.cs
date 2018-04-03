e6034a72527a043eeb57501915c6454bd11d78e8
sacitgonen <sacit.gonen41@myhunter.cuny.edu>
2018-04-03 16:00:18 -0400

    User entry gets created on User Table when new user registers

--- MusicApp/Assets/scripts/SignIn/EntryPage.cs
+++ MusicApp/Assets/scripts/SignIn/EntryPage.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Firebase;
 using Firebase.Auth;
-using UnityEngine;
 using Firebase.Unity.Editor;
 
 
--- MusicApp/Assets/scripts/SignIn/RegistrationPage.cs
+++ MusicApp/Assets/scripts/SignIn/RegistrationPage.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
+using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using Firebase;
 using Firebase.Auth;
-using UnityEngine;
 using Firebase.Unity.Editor;
 using Firebase.Database;
 
+public class User {
+	public string Name;
+	public string Last;
+	public int Level;
+
+	public User() {
+	}
+
+	public User(string f_name, string l_name) {
+		this.Name = f_name;
+		this.Last = l_name;
+		this.Level = 1;
+	}
+}
 
 namespace SignIn{
 	public class RegistrationPage : MonoBehaviour {
@@ -21,22 +35,24 @@ namespace SignIn{
 		void Start() {
 			FirebaseApp.DefaultInstance.SetEditorDatabaseUrl ("https://music-learning-capstone-c019b.firebaseio.com");
 		}
+
 		public void RegisterSubmit (string sceneName) {
 			FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(email.text, password.text).ContinueWith(obj => {
 				if (obj.IsCompleted) {
 					auth = Firebase.Auth.FirebaseAuth.GetAuth (FirebaseAuth.DefaultInstance.App);
 					user = auth.CurrentUser;
-					AddUser(user.UserId);
+					AddUser(fname.text, lname.text);
 					SceneManager.LoadSceneAsync (sceneName);
 				}
-				});
+			});
 		}
-
-		void AddUser(string id) {
-			DatabaseReference user_table = FirebaseDatabase.DefaultInstance.RootReference;
+			
+		private void AddUser(string f_name, string l_name) {
+			User new_user = new User(f_name, l_name);
+			DatabaseReference user_table = FirebaseDatabase.DefaultInstance.GetReference ("User Table");
 			if (user != null) {
-				string json = JsonUtility.ToJson (id);
-				user_table.Child ("User Table").Push ().SetRawJsonValueAsync (json);
+				string json = JsonUtility.ToJson(new_user);
+				user_table.Child (user.UserId).SetRawJsonValueAsync (json);
 			}
 		}
 	}
