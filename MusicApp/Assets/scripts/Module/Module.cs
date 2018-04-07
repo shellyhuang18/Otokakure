abef94299c5d0ea8540de360dc927fb8ff221e3b
Jack C <chen.jack@email.com>
2018-04-07 19:54:51 -0400

    created a folder and base class for Module (which is responsible for randomly generating songs

---
+++ MusicApp/Assets/scripts/Module/Module.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+//The base class used as a template for generating random
+public abstract class Module {
+
+	abstract public string generateSong ();
+
+}
