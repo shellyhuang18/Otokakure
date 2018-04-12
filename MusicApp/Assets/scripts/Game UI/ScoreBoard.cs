74892c8d9a3e87b03fe8176a83037b49c389d5a8
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2018-04-11 20:15:44 -0400

    Game window UI

---
+++ MusicApp/Assets/scripts/Game UI/ScoreBoard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour {
+	int score = 0;
+	public Text scoreBoard;
+	public Text percentBoard;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void IncrementScore(int value){
+		score += value;
+		scoreBoard.text = "Score: " + score;
+	}
+
+	void PercentageScore(int total){
+		double percentHit = score / total;
+		percentBoard.text = percentHit + "%";
+	}
+
+	void Progress(int amount, int total){
+		double progress = amount / total;
+	}
+}
