b346f16b9ff296034df4ecd5b1cf288027816283
Rubaiyat Rashid <Rubaiyat.Rashid51@myhunter.cuny.edu>
2018-04-11 21:39:50 -0400

    Progress bar

--- MusicApp/Assets/scripts/Game UI/ScoreBoard.cs
+++ MusicApp/Assets/scripts/Game UI/ScoreBoard.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using ProgressBar;
 
 public class ScoreBoard : MonoBehaviour {
 	int score = 0;
 	public Text scoreBoard;
 	public Text percentBoard;
+	public GameObject gameobj;
+	ProgressBarBehaviour BarBehaviour;
 	// Use this for initialization
 	void Start () {
-		
+		BarBehaviour = gameobj.GetComponent <ProgressBarBehaviour> ();
+		BarBehaviour.ProgressSpeed = 1000;
+		BarBehaviour.Value = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//How to use the functions
+		/*
+		Progress (prog, 100);
+		IncrementScore (1);
+		PercentageScore (100);
+		prog += 1;
+		*/
 	}
 
 	void IncrementScore(int value){
@@ -27,7 +38,11 @@ public class ScoreBoard : MonoBehaviour {
 		percentBoard.text = percentHit + "%";
 	}
 
+	//amount = how many passed
+	//total = how many there are
 	void Progress(int amount, int total){
 		double progress = amount / total;
+		BarBehaviour.Value = (float)progress * 100;
+		//BarBehaviour.SetFillerSizeAsPercentage((float)progress * 100);
 	}
 }
